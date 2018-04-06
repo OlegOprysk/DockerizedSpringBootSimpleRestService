b2b0e0d5e6b42d53d1207c5acdba980854d58a7a
oleg <olegoprysk@gmail.com>
2018-04-06 12:13:05 +0300

    Add dependencies, update Dockerfile, add settings.gradle file, adjust controller etc

--- src/main/java/com/olegoprysk/sbrestservice/controller/IndexController.java
+++ src/main/java/com/olegoprysk/sbrestservice/controller/IndexController.java
@@ -1,21 +1,40 @@
 package com.olegoprysk.sbrestservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@RefreshScope
 @RestController
 public class IndexController {
     public static final UUID uuid = UUID.randomUUID();
-    public static final String pattern = "UUID::%s</br>Variable::%s</br>";
+    public static final String pattern = "UUID::%s</br>" +
+            "Variable_perProfile::%s</br>" +
+            "Variable_common::%s</br>" +
+            "Profile::%s</br>" +
+            "JSON::</br>%s";
 
-    @Value("${test.prop:thisISDefaultValue}")
+    @Autowired
+    private Environment environment;
+
+    @Value("${app.test.prop:test}")
     public String variable;
+
+    @Value("${app.common.variable:test}")
+    public String commonVariable;
+
+
+    @Value("${config:{}}")
+    public String jsonFile;
 
     @RequestMapping("/")
     public String index(){
-        return String.format(pattern, uuid, variable);
+        String[] activeProfiles = environment.getActiveProfiles();
+        return String.format(pattern, uuid, variable, commonVariable,  activeProfiles.length > 0 ? activeProfiles[0]:"none", jsonFile);
     }
 }
--- src/test/java/com/olegoprysk/sbrestservice/SbRestServiceApplicationTests.java
+++ src/test/java/com/olegoprysk/sbrestservice/SbRestServiceApplicationTests.java
@@ -2,15 +2,23 @@ package com.olegoprysk.sbrestservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class SbRestServiceApplicationTests {
 
+    @Autowired
+    private Environment environment;
     @Test
     public void contextLoads() {
+        System.out.println("------------->>>>>  "+ environment.getActiveProfiles());
     }
 
 }
